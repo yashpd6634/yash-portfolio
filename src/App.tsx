33a2b9a76883d83f9983e6679e841802b745dfd4
Yash Dewangan <yashpd13@gmail.com>
2025-10-06 01:08:17 +0530

    completed starting animated project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Features from "./components/Features/Features";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Story from "./components/Story/Story";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
-      
-      <section className="z-0 min-h-screen bg-blue-500"></section>
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   );
 };
