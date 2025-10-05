aec60deb3407d7f724bcc4778751ee5015eafd26
Yash Dewangan <yashpd13@gmail.com>
2025-10-05 21:32:42 +0530

    added hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
+import Hero from "./components/Hero/Hero";
+
 const App = () => {
   return (
-    <main className="min-h-screen bg-blue-50 p-8">
-      <h1 className="text-5xl font-zentry text-blue-300 mb-4">
-        Zentry Font Test
-      </h1>
-      <p className="text-2xl font-general text-violet-300 mb-4">
-        General Font Test
-      </p>
-      <p className="text-xl font-circular-web text-yellow-300">
-        Circular Web Font Test
-      </p>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+      
+      <section className="z-0 min-h-screen bg-blue-500"></section>
     </main>
   );
 };
