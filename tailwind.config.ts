31c0e4f21c665de7c2a54469531266d1dd6dc413
Yash Dewangan <yashpd13@gmail.com>
2025-10-06 07:10:26 +0530

    added extra theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
+        sans: ["Mona Sans", "sans-serif"],
         zentry: ["zentry", "sans-serif"],
         general: ["general", "sans-serif"],
         "circular-web": ["circular-web", "sans-serif"],
@@ -12,8 +13,18 @@ export default {
         "robert-regular": ["robert-regular", "sans-serif"],
       },
       colors: {
+        white: {
+          DEFAULT: "#ffffff", // Standard white
+          50: "#d9ecff",
+        },
+        black: {
+          DEFAULT: "#000000", // Standard black
+          50: "#1c1c21",
+          100: "#0e0e10",
+          200: "#282732",
+        },
         blue: {
-          50: "#dfdff0",
+          50: "#839cb5",
           75: "#dfdff2",
           100: "#f0f2fa",
           200: "#010101",
