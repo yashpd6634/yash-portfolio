6691ab6706eab87e057ee439cd6bced0f6b00870
Yash Dewangan <yashpd13@gmail.com>
2025-10-07 11:27:27 +0530

    added logos

--- src/components/sections/Contact/Contact2.tsx
+++ src/components/sections/Contact/Contact2.tsx
@@ -104,7 +104,7 @@ const Contact = () => {
                       {loading ? "Sending..." : "Send Message"}
                     </p>
                     <div className="arrow-wrapper">
-                      <img src="/images/arrow-down.svg" alt="arrow" />
+                      <img src="/img/arrow-down.svg" alt="arrow" />
                     </div>
                   </div>
                 </button>
